01fd3a7d61ffe9c85b7bc2c8046ab5015ba0cdb8
macintoshplus <jean-baptiste@nahan.fr>
2016-02-05 17:19:08 +0100

    update for multi install

--- src/JbNahan/PhpUpdate/Command/CurrentVersionCommand.php
+++ src/JbNahan/PhpUpdate/Command/CurrentVersionCommand.php
@@ -12,6 +12,7 @@ namespace JbNahan\PhpUpdate\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class CurrentVersionCommand extends Command
 {

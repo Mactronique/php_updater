a8c926e0e858ca69e978738f3e5d68f0cdc760ca
macintoshplus <jean-baptiste@nahan.fr>
2016-09-30 13:40:10 +0200

    fixes #4

--- src/JbNahan/PhpUpdate/Manager/PHPSourceManager.php
+++ src/JbNahan/PhpUpdate/Manager/PHPSourceManager.php
@@ -11,48 +11,48 @@ namespace JbNahan\PhpUpdate\Manager;
 
 class PHPSourceManager
 {
-	private $master;
-	private $archives;
-	private $versions;
+    private $master;
+    private $archives;
+    private $versions;
 
-	public function __construct($master, $archives, array $versions)
-	{
-		$this->master = $master;
-		$this->archives = $archives;
-		$this->versions = $versions;
-	}
+    public function __construct($master, $archives, array $versions)
+    {
+        $this->master = $master;
+        $this->archives = $archives;
+        $this->versions = $versions;
+    }
 
-	public function latestVersionForBranch($branch)
-	{
-		$branch_ver = $this->versions[$branch];
+    public function latestVersionForBranch($branch)
+    {
+        $branch_ver = $this->versions[$branch];
 
-        krsort($branch_ver);
-        return current($branch_ver);
-	}
+        natsort($branch_ver);
+        return end($branch_ver);
+    }
 
-	public function zipNameForVersion($branch, $version)
-	{
-		if(!$this->branchExists($branch)){
-			return null;
-		}
-		$branch_ver = $this->versions[$branch];
-		if(!array_key_exists($version, $branch_ver)){
-			return null;
-		}
-		return $branch_ver[$version];
-	}
+    public function zipNameForVersion($branch, $version)
+    {
+        if (!$this->branchExists($branch)) {
+            return null;
+        }
+        $branch_ver = $this->versions[$branch];
+        if (!array_key_exists($version, $branch_ver)) {
+            return null;
+        }
+        return $branch_ver[$version];
+    }
 
-	public function branchExists($branch)
-	{
-		return array_key_exists($branch, $this->versions);
-	}
+    public function branchExists($branch)
+    {
+        return array_key_exists($branch, $this->versions);
+    }
 
-	public function master()
-	{
-		return $this->master;
-	}
-	public function archives()
-	{
-		return $this->archives;
-	}
+    public function master()
+    {
+        return $this->master;
+    }
+    public function archives()
+    {
+        return $this->archives;
+    }
 }
